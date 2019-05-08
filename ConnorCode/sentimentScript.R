c18127aebab0693ce7e4fe6addb75bac5794cc5e
Connor Rothschild <connorrothschild@Connors-MacBook-Pro.local>
2019-05-08 14:24:38 -0500

    took plots out of sentiment script, made it more specifically for lexicon matching etc.

--- ConnorCode/sentimentScript.R
+++ ConnorCode/sentimentScript.R
@@ -29,20 +29,10 @@ tweets_w_sentiment <- tweets %>%
 
 # extract the keywords responsible for sentiment coding??
 # below is not working; i need it to keep candidate name but its extracitng sentiment terms and keeping nothing else
-#tweets_w_sentiment <- tweets_w_sentiment %>% 
+# tweets_w_sentiment <- tweets_w_sentiment %>% 
  # mutate(keywords <- extract_sentiment_terms(tweets$text, polarity_dt = lexicon::hash_sentiment_jockers_rinker))
 
 # sentiment_terms <- extract_sentiment_terms(tweets$text, polarity_dt = lexicon::hash_sentiment_jockers_rinker)
-
-# average sentiment by candidate
-candidate_sentiment <- tweets_w_sentiment %>%
-  group_by(candidate) %>% 
-  summarise(sentiment = mean(sentiment)) %>% 
-  arrange(desc(sentiment)) %>% 
-  ggplot(aes(x = reorder(candidate, sentiment), y = sentiment, fill=candidate)) +
-  geom_col()
-
-candidate_sentiment
 
 # specific word analysis
 
@@ -74,42 +64,4 @@ by_candidate_sentiment <- tweet_words %>%
 
 head(by_candidate_sentiment)
 ## what the above tells us is a candidates' tweets' proportion of words that are of a certain sentiment
-# e.g. 3.96% of Amy Klobuchar's "Tweet words" are angry, 6.93% are in anticipation, etc.
-
-# specific emotion/sentiment by candidate
-by_candidate_sentiment_grouped <- by_candidate_sentiment %>% 
-  group_by(sentiment) %>% 
-ggplot(aes(x=reorder(sentiment,percent),y=percent, fill=sentiment)) +
-  geom_col() +
-  theme(axis.text.x=element_blank(),axis.ticks=element_blank()) +
-  facet_wrap(~candidate) +
-  ggtitle("Proportional Sentiment per Candidate") +
-  xlab(element_blank())+
-  ylab("Percent")
-
-by_candidate_sentiment_grouped
-
-# the same thing but grouped by emotion with each candidate having their own bar
-by_sentiment_candidate_grouped <- by_candidate_sentiment %>% 
-  group_by(candidate) %>% 
-  ggplot(aes(x=candidate,y=percent, fill=candidate)) +
-  geom_col() +
-  theme(axis.text.x=element_blank(),axis.ticks=element_blank()) +
-  facet_wrap(~sentiment) +
-  ggtitle("Proportional Sentiment per Candidate") +
-  xlab(element_blank())+
-  ylab("Percent")
-
-by_sentiment_candidate_grouped
-
-# interaction between sentiment and Tweet interactions
-
-polarity_vs_popularity <- tweets_w_sentiment %>%
-  group_by(candidate) %>% 
-  filter(favoriteCount<60000) %>% 
-  ggplot(aes(x=sentiment, y=favoriteCount, color=candidate)) +
-  geom_point() +
-  geom_smooth(method=lm, se=FALSE)
-
-polarity_vs_popularity
-
+# e.g. 3.44% of Amy Klobuchar's "Tweet words" are angry, 6.56% are in anticipation, etc.
