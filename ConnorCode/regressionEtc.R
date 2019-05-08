601870296990b50f44ca16f9d6dec4d5a8653eb5
Connor Rothschild <connorrothschild@Connors-MacBook-Pro.local>
2019-05-08 14:24:19 -0500

    added new regression

--- ConnorCode/regressionEtc.R
+++ ConnorCode/regressionEtc.R
@@ -1,4 +1,4 @@
-### STEP THREE: REGRESSION AND STATISTICS STUFF
+### STEP FOUR: REGRESSION AND STATISTICS STUFF
 ### TO DO ONCE EDA IS COMPLETED AND SENTIMENT HAS BEEN COMPUTED
 ### Reference https://stats.idre.ucla.edu/r/dae/poisson-regression/
 
@@ -21,6 +21,10 @@ library(broom)
 # also reference http://varianceexplained.org/r/trump-tweets/
 sentiment_differences <- tweets_w_sentiment %>%
   group_by(candidate) %>%
-  do(tidy(poisson.test(.$candidate, .$sentiment)))
+  do(tidy(poisson.test(tweets_w_sentiment$candidate, tweets_w_sentiment$sentiment)))
 
 sentiment_differences
+
+# interaction between sentiment and favs/rts
+summary(lm(favoriteCount ~ sentiment, data=tweets_w_sentiment))
+summary(lm(retweetCount ~ sentiment, data=tweets_w_sentiment))
