a8f7d59d34f7d66178078ca7120f1fa778994d32
Connor Rothschild <connorrothschild@Connors-MacBook-Pro.local>
2019-05-10 11:46:38 -0500

    added plot for Tweets over time; cleaned other plots; added labels

--- ConnorCode/plotsAndAnalysis.R
+++ ConnorCode/plotsAndAnalysis.R
@@ -24,17 +24,37 @@ tweets_per_candidate <- tweets_w_sentiment %>%
 
 tweets_per_candidate
 
+#tweets over time
+tweets_over_time <- tweets %>% 
+  ggplot(aes(x=month(createdDate))) +
+  geom_histogram(position = "identity", bins = 10, show.legend = FALSE)
+  
+tweets_over_time  
+  
+# by candidate
+ggplot(tweets, aes(x = month(createdDate), fill = candidate)) +
+  geom_histogram(position = "identity", bins = 10, show.legend = FALSE) +
+  facet_wrap(~candidate, scales = "free")
+
+# takeaway: Andrew Yang Tweets way more than everyone else, 
+# explaining why almost all of his tweets (1500) are from 
+# the last couple months (March)
+
 # average sentiment by candidate
 candidate_sentiment <- tweets_w_sentiment %>%
   group_by(candidate) %>% 
   summarise(sentiment = mean(sentiment)) %>% 
   arrange(desc(sentiment)) %>% 
   ggplot(aes(x = reorder(candidate, sentiment), y = sentiment, fill=candidate)) +
-  geom_col()
+  geom_col(show.legend = FALSE) +
+  xlab("Candidate") +
+  ylab(element_blank()) +
+  ggtitle("Average Sentiment by Candidate", subtitle = "With higher scores indicating more positive content") 
 
 candidate_sentiment
 
 # specific emotion/sentiment by candidate
+legend_ord <- levels(with(by_candidate_sentiment, reorder(sentiment, -percent)))
 by_candidate_sentiment_grouped <- by_candidate_sentiment %>% 
   group_by(sentiment) %>% 
   ggplot(aes(x=reorder(sentiment,percent),y=percent, fill=sentiment)) +
@@ -43,7 +63,8 @@ by_candidate_sentiment_grouped <- by_candidate_sentiment %>%
   facet_wrap(~candidate) +
   ggtitle("Proportional Sentiment per Candidate") +
   xlab(element_blank())+
-  ylab("Percent")
+  ylab("Percent") +
+  scale_fill_discrete(name="Sentiment", breaks = legend_ord)
 
 by_candidate_sentiment_grouped
 
@@ -53,10 +74,11 @@ by_sentiment_candidate_grouped <- by_candidate_sentiment %>%
   ggplot(aes(x=candidate,y=percent, fill=candidate)) +
   geom_col() +
   theme(axis.text.x=element_blank(),axis.ticks=element_blank()) +
-  facet_wrap(~sentiment) +
+  facet_wrap(~sentiment, scales = "free") +
   ggtitle("Proportional Sentiment per Candidate") +
   xlab(element_blank())+
-  ylab("Percent")
+  ylab("Percent") +
+  scale_fill_discrete(name="Candidate")
 
 by_sentiment_candidate_grouped
 
@@ -66,7 +88,10 @@ polarity_vs_popularity_fav <- tweets_w_sentiment %>%
   filter(favoriteCount<60000) %>% 
   ggplot(aes(x=sentiment, y=favoriteCount)) +
   geom_point(alpha = .05) +
-  geom_smooth(method=lm) 
+  geom_smooth(method=lm) +
+  ylab("Number of Favorites") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Favorites")
 
 polarity_vs_popularity_fav
 
@@ -76,7 +101,11 @@ polarity_vs_popularity_fav_by_candidate <- tweets_w_sentiment %>%
   group_by(candidate) %>% 
   filter(favoriteCount<60000) %>% 
   ggplot(aes(x=sentiment, y=favoriteCount, color=candidate)) +
-  geom_point(alpha = .25)
+  geom_point(alpha = .25) +
+  ylab("Number of Favorites") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Favorites", 
+          subtitle = "Broken down by candidate")
 
 polarity_vs_popularity_fav_by_candidate
 
@@ -87,8 +116,12 @@ polarity_vs_popularity_fav_faceted <- tweets_w_sentiment %>%
   filter(favoriteCount<38000) %>% 
   ggplot(aes(x=sentiment, y=favoriteCount)) +
   geom_point(alpha = .1) +
-  facet_wrap(~candidate) +
-  geom_smooth(method=lm) 
+  facet_wrap(~candidate, scales = "free") +
+  geom_smooth(method=lm) +
+  ylab("Number of Favorites") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Favorites", 
+          subtitle = "Faceted by candidate")
 
 polarity_vs_popularity_fav_faceted
 
@@ -99,7 +132,12 @@ polarity_vs_popularity_fav_summarised <- tweets_w_sentiment %>%
   summarise(meansentiment = mean(sentiment), meanfavorite=mean(favoriteCount)) %>% 
   ggplot(aes(x=meansentiment, y=meanfavorite, color=candidate)) +
   geom_point() +
-  geom_label_repel(aes(label=candidate),show.legend = F)
+  geom_label_repel(aes(label=candidate),show.legend = F) +
+  ylab("Average Number of Favorites") +
+  xlab("Average Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Favorites", 
+          subtitle = "Per Candidate") +
+  scale_fill_discrete(label="Candidate")
 
 polarity_vs_popularity_fav_summarised
 
@@ -109,7 +147,10 @@ polarity_vs_popularity_rt <- tweets_w_sentiment %>%
   filter(retweetCount<38000) %>% 
   ggplot(aes(x=sentiment, y=retweetCount)) +
   geom_point(alpha = .1) +
-  geom_smooth(method=lm) 
+  geom_smooth(method=lm) +
+  ylab("Number of Retweets") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Retweets")
 
 polarity_vs_popularity_rt
 
@@ -119,7 +160,11 @@ polarity_vs_popularity_rt_by_candidate <- tweets_w_sentiment %>%
   group_by(candidate) %>% 
   filter(retweetCount<38000) %>% 
   ggplot(aes(x=sentiment, y=retweetCount, color=candidate)) +
-  geom_point(alpha = .25)
+  geom_point(alpha = .25) +
+  ylab("Number of Retweets") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Retweets",
+          subtitle = "Broken down by candidate")
 
 polarity_vs_popularity_rt_by_candidate
 
@@ -131,7 +176,11 @@ polarity_vs_popularity_rt_faceted <- tweets_w_sentiment %>%
   ggplot(aes(x=sentiment, y=retweetCount)) +
   geom_point(alpha = .1) +
   facet_wrap(~candidate) +
-  geom_smooth(method=lm) 
+  geom_smooth(method=lm) +
+  ylab("Number of Retweets") +
+  xlab("Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Retweets",
+          subtitle = "Faceted by candidate")
 
 polarity_vs_popularity_rt_faceted
 
@@ -142,6 +191,11 @@ polarity_vs_popularity_rt_summarised <- tweets_w_sentiment %>%
   summarise(meansentiment = mean(sentiment), meanretweet=mean(retweetCount)) %>% 
   ggplot(aes(x=meansentiment, y=meanretweet, color=candidate)) +
   geom_point() +
-  geom_label_repel(aes(label=candidate),show.legend = F)
+  geom_label_repel(aes(label=candidate),show.legend = F) +
+  ylab("Average Number of Retweets") +
+  xlab("Average Sentiment Score") +
+  ggtitle("Relationship Between Tweet Sentiment and Retweets",
+          subtitle = "Per candidate") +
+  scale_fill_discrete(label="Candidate")
 
 polarity_vs_popularity_rt_summarised
